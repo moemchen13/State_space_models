3f1a888d7a94b5f3f1edc25c2a4a105986890be1
moemchen13 <super.moritz.christ@gmx.de>
2025-10-09 16:09:09 +0200

    Fast implementation of the S4 model finished

--- mamba.py
+++ mamba.py
@@ -10,7 +10,7 @@ class S4_with_shared_A(nn.Module):
     #Advantage less params mixture of signal in latent space possible
     def __init__(self,channels: int,hidden_state: int = 64,min_delta: float = 1e-3,
                 max_delta:float=0.1,kernel_max_size:int=1,eps: float = 1e-6,mode:str="Conv_FFT",
-                use_hippo:bool=True,seed=42,persist_cache=True):
+                use_hippo:bool=True,seed=42,persist_cache=True,**kwargs):
         super().__init__()
         D,N = channels,hidden_state
         self.D,self.N = D,N
@@ -130,10 +130,10 @@ class S4_with_shared_A(nn.Module):
     
 
 
-class S4_base(nn.Module):
+class S4_naive(nn.Module):
     def __init__(self,hidden_state: int = 64,min_delta: float = 1e-3,
                 max_delta:float=0.1,kernel_max_size:int=1,eps: float = 1e-6,mode:str="Conv_FFT",
-                use_hippo:bool=True,seed=42,persist_cache=True):
+                use_hippo:bool=True,seed=42,persist_cache=True,**kwargs):
         super().__init__()
         N = hidden_state
         self.N = N
@@ -171,9 +171,12 @@ class S4_base(nn.Module):
             idx = torch.arange(N)
             init = torch.sqrt(2*idx+1).unsqueeze(1)
             HiPPO = init @ init.T
+            #print(HiPPO)
+            #print("/n")
             row, col = torch.triu_indices(HiPPO.size(0), HiPPO.size(1), offset=1)
             HiPPO[row,col] = 0
             HiPPO[torch.arange(N),torch.arange(N)] -= idx
+            HiPPO = -HiPPO
             tensor.copy_(HiPPO)
         return tensor
 
@@ -190,6 +193,8 @@ class S4_base(nn.Module):
         nn.init.ones_(self.D)
         nn.init.uniform_(self.log_delta,a=torch.log(self.min_delta),b=torch.log(self.max_delta))
         
+        """
+        #Hurwitz for numerical stability for high N and L values -still needed
         with torch.no_grad():
             I = torch.eye(self.N, device=self.A.device, dtype=self.A.dtype)
             # scale to avoid huge norms
@@ -197,7 +202,7 @@ class S4_base(nn.Module):
             self.A.div_(anorm.clamp_min(1e-6))
             # shift eigenvalues to Re(λ) < 0
             self.A.add_(-0.1 * I) 
-        #Hurwitz for numerical stability for high N and L values
+        """
 
 
     def reset_hidden_state(self, batch_size: int | None = None, *, device=None, dtype=None):
@@ -302,16 +307,164 @@ class S4_base(nn.Module):
         return y.squeeze(0) if X_in.dim() == 1 else y
     
 
+class S4_fast(S4_naive):
+    def __init__(self,hidden_state:int=64,use_ortho_pq:bool=True,rank:int=1,**kwargs):
+        super().__init__(hidden_state=hidden_state,**kwargs)
+        self.r = rank
+        N = hidden_state
+
+        self.theta = nn.Parameter(torch.empty(N)) #real valued part of D
+        self.omega = nn.Parameter(torch.empty(N)) #imaginary part of D
+        self.Q = nn.Parameter(torch.empty(N,self.r))
+        self.P = nn.Parameter(torch.empty(N,self.r))
+
+        self.init_matrices(use_ortho_pq=use_ortho_pq)
+
+
+    def init_matrices(self,use_ortho_pq:bool):
+        #initializing the sparse matrices for A and B,C
+        with torch.no_grad():
+            n = torch.arange(self.N,dtype=torch.float32)
+            base = torch.sqrt(2*n+1).unsqueeze(-1)
+            self.theta.copy_(torch.log(n+1.0))
+            self.omega.zero_()
+
+            P0 = base.repeat(1,self.r) #not restricted in original S4 model to same 
+            Q0 = base.repeat(1,self.r) #P!=Q possible
+
+            if self.r>1 and use_ortho_pq:
+                P0[:,1:] += 1e-3*torch.randn(self.N,self.r-1)
+                Q0[:,1:] += 1e-3*torch.randn(self.N,self.r-1)
+                
+            self.P.copy_(P0/(self.r **0.5))
+            self.Q.copy_(Q0/(self.r **0.5))
+            nn.init.normal_(self.B,std=(1.0/self.N)**0.5)
+            nn.init.normal_(self.C,std=1.0)
+
+    @property
+    def D_diag(self):
+        #similar to delta but now with constructed diagonal unitary D matrix
+        return -torch.exp(self.theta)+ 1j* self.omega
+    
+    def full_A(self):
+        D = torch.diag(self.D_diag.to(self.P.dtype).to(self.P.device))
+        Q = self.Q
+        if Q.dim>2:
+            #else throws error
+            Q = Q.T
+        A = D+(self.P @ Q.to(D.dtype))
+        return A
+    
+
+    def kernel(self,L:int):
+        #fast computation of the kernel filter via sampling the generating function
+        #L=kernel_length >=1
+        assert L>0
+        import torch
+        device = self.B.device
+        dtypeR = self.B.dtype #either 32 or 64 
+        dtypeC = torch.complex64 if dtypeR == torch.float32 else torch.complex128
+        
+        #1)frequencies z_m
+        k = torch.arange(L,device=device,dtype=dtypeR)
+        w = torch.pi*2 *k/L
+        z = torch.exp(1j * w.to(dtypeC)) #frequency samples on the complex unit circle
+
+        #2)bilinear map: s_k
+        delta = self.delta.to(device=device,dtype=dtypeR)
+        s = (2.0/delta).to(dtypeC) * (z-1.0)/(z+1.0) #each frequency z has continous freq s
+        
+
+        #3)create D and shapes complex and on correct device
+        D = self.D_diag.to(device=device,dtype=dtypeC)
+        B = self.B.to(device=device,dtype=dtypeC)
+        C = self.C.to(device=device,dtype=dtypeC)
+        P = self.P.to(device=device,dtype=dtypeC)
+        Q = self.Q.to(device=device,dtype=dtypeC)
+        
+        #4)compute resolvent (sI-A)⁻¹B efficient
+        R = (s[:,None]-D[None,:]) #R = sI-D #shape: [L,N]
+        
+        #Sherman-Morrison-Woodbury (sI-(D+PQ.T))⁻¹ = R⁻¹-R⁻¹P(I+Q.T R⁻¹P)⁻¹Q.T R⁻¹
+        RB = (B.squeeze(-1)[None,:]/R) #nominator B #shape: [L,N]
+        RP = (P[None,:,:]/R[...,None]) #nominator P #shape: [L,N,r]
+        
+        QT_RP = torch.einsum('bi,jbk->jki',Q,RP) #shape: [L,r,r] might need 'bi,jbk->jik'
+        I = torch.eye(self.r,device=device,dtype=dtypeC).expand(L,-1,-1)#shape: [L,r,r]
+        S = I+QT_RP #shape: [L,r,r]
+
+        
+        #Solve system t= S⁻¹Q.T RB (one for each freq)
+        QT_RB = torch.einsum('bi,jb->ji',Q,RB) #shape: [L,r]
+        t = torch.linalg.solve(S,QT_RB.unsqueeze(-1)) #low-rank correction term #shape [L,r,1] 
+        
+        RP_t = torch.einsum('lnk,lri->ln',RP,t) # shape: [L,N]
+        x = RB-RP_t # shape: [L,N]
+        
+        #4)K(z_k) = C*x_m
+        Kz = torch.einsum('rn,ln->l',C,x) #shape: [L]
+        #5)iFFT into time domain kernel K
+        K_time = torch.fft.ifft(Kz,n=L).real
+
+        return K_time.view(1,1,L).to(self.B.dtype)
+
+
+    def discretize(self):
+        #only used for the RNN way therefore dense representation of A
+        A = self.full_A()  # complex
+        I = torch.eye(self.N, device=A.device, dtype=A.dtype)
+        delta = self.delta.to(device=A.device, dtype=A.dtype)
+        A1 = I - delta * 0.5 * A
+        A2 = I + delta * 0.5 * A
+        Ad = torch.linalg.solve(A1, A2)                 #[N,N] (complex)
+        Bd = torch.linalg.solve(A1, delta * self.B.to(A.dtype))  #[N,1] (complex)
+        if Ad.is_complex():
+            Ad_r = Ad.real
+            Bd_r = Bd.real
+            return Ad_r, Bd_r
+        return Ad, Bd
+
+
+
+    def propagate_convolution_filter(self,X,use_fourier=True):
+        X_in = X
+        if X.dim() == 1:
+            X = X.unsqueeze(0)
+        B,L = X.shape
+
+        K = self.kernel(min(L,self.kernel_max_size))
+
+        K_len = K.shape[-1]
+        end_size = L+ K_len -1
+
+        X_pad = nn.functional.pad(X,(0,end_size-L))
+        K_pad = nn.functional.pad(K.squeeze(0),(0,end_size - K_len))
+
+        if use_fourier:
+            Xd = torch.fft.rfft(X_pad,n=end_size,dim=-1)
+            Kd = torch.fft.rfft(K_pad,n=end_size,dim=-1)
+            Yd = Xd*Kd
+            y = torch.fft.irfft(Yd,n=end_size,dim=-1)[...,:L]
+        else:
+            X3 = X.unsqueeze(1)
+            y = nn.functional.conv1d(X3,K,padding=K_len).squeeze(1)[...,:L]
+
+        return y.squeeze(0) if X_in.dim() == 1 else y
+
 class MultiChannelS4(nn.Module):
     """
     Wrap a S4 module into D parallel, indepent channels 
     This is not the most efficient as we dont parallelize D
     """
-    def __init__(self, D, *base_args, **base_kwargs):
+    def __init__(self, n_channels:int=1,implementation:str= "naive", *base_args, **base_kwargs):
         super().__init__()
-        self.D = D
+        self.n_channels = n_channels
         # D independent copies (each has its own params)
-        self.channels = nn.ModuleList([S4_base(*base_args, **base_kwargs) for _ in range(self.D)])
+        if implementation=="naive":
+            self.channels = nn.ModuleList([S4_naive(*base_args, **base_kwargs) for _ in range(self.n_channels)])
+        else:
+            self.channels = nn.ModuleList([S4_fast(*base_args, **base_kwargs) for _ in range(self.n_channels)])
+        
 
     def forward(self, x):
         
@@ -328,7 +481,7 @@ class MultiChannelS4(nn.Module):
             X = x
         B, D, L = X.shape
 
-        assert D == self.D, f"Expected {self.D} channels, got {D}"
+        assert D == self.n_channels, f"Expected {self.n_channels} channels, got {D}"
         y = torch.empty_like(X)
         for d,channel in enumerate(self.channels):
             y[:,d,:]= channel(X[:,d,:])
